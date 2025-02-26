145f4e08743c8637b06d0fa057ecd661c956222c
Leland Capps <lcapps5600@gmail.com>
2025-02-26 17:17:56 -0500

    strings, concatination, variables, print

---
+++ Day1.py
@@ -0,0 +1,24 @@
+print("hello world\nhello world\nHello world")
+
+print("Hello world!")
+
+print("My name is" + " " + "Angela" + "!")
+
+glass1 = "milk"
+glass2 = "juice"
+
+glass3=glass1
+glass1=glass2
+glass2=glass3
+
+print(glass1, glass2)
+
+name = "Angela"
+length = len(name)
+print(length)
+
+
+print("Welcome to the band name generator")
+city = input("What city did you grow up in?\n")
+pet = input("Whats the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
